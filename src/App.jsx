d35241845e15be700fa625181b8dafded9b47277
PochienTsai <bernie7135@gmail.com>
2025-01-28 21:51:15 +0800

    新增 Bootstrap 和 Sass 依賴，並導入全局樣式

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
