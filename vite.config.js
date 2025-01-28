df97c9b5ed9f5236b05f38997aa085fb8f75676b
PochienTsai <bernie7135@gmail.com>
2025-01-28 23:14:42 +0800

    更新 API 路徑至環境變數，調整預覽命令以自動開啟，並新增環境配置檔(env.development or env.production)。

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   console.log(`Building in mode: ${mode}`); // 用於調試，查看當前模式
   return {
-    // 設定 dist 資料夾中檔案的路徑關係
+    // 設定 dist 資料夾中檔案的路徑關係(後續公司部署至IIS就可將base不管生產或開發環境皆設定為'/')
     // - 生產環境：設定 GitHub Pages 的路徑為 '/vite_test/'
     // - 開發環境及其他部署（如部署至 IIS）：使用相對路徑 '/'
     base: mode === 'production' ? '/vite_test/' : '/', // 設定基本路徑
