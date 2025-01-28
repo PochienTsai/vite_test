25625f0b0bb7e6fd10cf4c63c5a4299dccfaf8ee
PochienTsai <bernie7135@gmail.com>
2025-01-28 22:07:59 +0800

    新增 Bootstrap 模態框，並實現開啟功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-
+import { Modal } from 'bootstrap';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const modalRef = useRef(null);
+  const customMoal = useRef(null);
   useEffect(() => {
     (async () => {
       const res = await axios.get('https://randomuser.me/api/');
       console.log(res);
     })();
   }, []);
+
+  useEffect(() => {
+    console.log(modalRef.current);
+    customMoal.current = new Modal(modalRef.current);
+    // customMoal.current.show();
+  }, []);
+  const openModal = () => {
+    customMoal.current.show();
+  };
+
   return (
     <>
+      <button type='button' className='btn btn-primary' onClick={openModal}>
+        Launch demo modal
+      </button>
+
+      <div className='modal fade' ref={modalRef} tabIndex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h1 className='modal-title fs-5'>Modal title</h1>
+              <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div className='modal-body'>...</div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-secondary' data-bs-dismiss='modal'>
+                Close
+              </button>
+              <button type='button' className='btn btn-primary'>
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div>
         <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
